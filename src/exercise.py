52967f4e9e18e320af20916ef9f4d7ebef67b7db
Jake Lee <jake.lee@fdmgroup.com>
2021-09-02 11:45:14 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    sum = 0
+    while True:
+        num = int(input("Give a number:"))
+        if (num != 0):
+            sum += num
+        else:
+            break
+    print("Sum of numbers: " + str(sum))
 
 if __name__ == '__main__':
     main()
